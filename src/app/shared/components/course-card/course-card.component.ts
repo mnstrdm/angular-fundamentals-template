0f8d5129487727a29b0166f738ceecae183a83f3
“mnstrdm“ <mnstrdm@gmail.com>
2024-09-29 23:42:54 +0200

    add @output for showcourse

--- src/app/shared/components/course-card/course-card.component.ts
+++ src/app/shared/components/course-card/course-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from "@angular/core";
+import { Component, EventEmitter, Input, Output } from "@angular/core";
 interface Course {
   id: string;
   title: string;
@@ -16,6 +16,11 @@ export class CourseCardComponent {
   @Input() isEditabel!: boolean;
   @Input() course!: Course;
 
+  @Output() clickOnShow: EventEmitter<string> = new EventEmitter();
+
+  onShowCourse() {
+    this.clickOnShow.emit(this.course.id);
+  }
   /* title = this.course.title;
   description = this.course.description;
   authors = this.course.authors.join(", ");
