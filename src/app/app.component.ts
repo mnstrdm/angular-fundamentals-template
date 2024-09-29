b690fea5bbcb4700a6dc16a31664d9b27d79b70d
“mnstrdm“ <mnstrdm@gmail.com>
2024-09-29 20:29:23 +0200

    add user name and button to the right side

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import {
+  faTrash,
+  faPencil,
+  IconDefinition,
+  faTrashAlt,
+  faTrashCan,
+} from "@fortawesome/free-solid-svg-icons";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'courses-app';
+  title = "courses-app";
+  btnTextLogin: string = "Login";
+  btnTextLogout: string = "Logout";
+
+  faTrashCan: IconDefinition = faTrashCan;
+
+  faPencil: IconDefinition = faPencil;
 }
