b26760632057f1cd9648695deb4b862beaf13415
“mnstrdm“ <mnstrdm@gmail.com>
2024-09-29 21:14:46 +0200

    adding some variables required for app-info

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,10 +14,17 @@ import {
 })
 export class AppComponent {
   title = "courses-app";
+
+  //  text for buttons
   btnTextLogin: string = "Login";
   btnTextLogout: string = "Logout";
+  btnTextAddNewCourse: string = "Add New Course";
 
+  // icons for buttons
   faTrashCan: IconDefinition = faTrashCan;
-
   faPencil: IconDefinition = faPencil;
+
+  // info page text
+  infoTitle = "Your List Is Empty";
+  infoText = "Please use 'Add New Course' button to add your first course";
 }
