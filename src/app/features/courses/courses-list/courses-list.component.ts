f5ad952016e0c1a5c04fa1782f285a15bb46ceda
“mnstrdm“ <mnstrdm@gmail.com>
2024-09-29 23:59:37 +0200

    add @inputs and @outputs

--- src/app/features/courses/courses-list/courses-list.component.ts
+++ src/app/features/courses/courses-list/courses-list.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
-
+import { Component, EventEmitter, Input, Output } from "@angular/core";
+interface Course {
+  id: string;
+  title: string;
+  description: string;
+  creationDate: string;
+  duration: number;
+  authors: string[];
+}
 @Component({
-  selector: 'app-courses-list',
-  templateUrl: './courses-list.component.html',
-  styleUrls: ['./courses-list.component.css']
+  selector: "app-courses-list",
+  templateUrl: "./courses-list.component.html",
+  styleUrls: ["./courses-list.component.css"],
 })
 export class CoursesListComponent {
+  @Input() courses!: Course[];
+  @Input() editable!: boolean;
+  @Output() showCourse = new EventEmitter<string>();
+  @Output() editCourse = new EventEmitter<string>();
+  @Output() deleteCourse = new EventEmitter<string>();
 
 }
