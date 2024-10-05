5b7b9f7f8adcd5f0a89f4f95b6fddb9d61e80232
“mnstrdm“ <mnstrdm@gmail.com>
2024-10-05 19:04:45 +0200

    create email validation directive logic

--- src/app/shared/directives/email.directive.ts
+++ src/app/shared/directives/email.directive.ts
@@ -1,9 +1,37 @@
 import { Directive } from "@angular/core";
+import {
+  AbstractControl,
+  NG_VALIDATORS,
+  ValidationErrors,
+  Validator,
+  ValidatorFn,
+} from "@angular/forms";
 
 @Directive({
-    selector: '[emailValidator]',
-    providers: [/*Add your code here*/]
+  selector: "[emailValidator]",
+  providers: [
+    {
+      provide: NG_VALIDATORS,
+      useExisting: EmailValidatorDirective,
+      multi: true,
+    },
+  ],
 })
-export class EmailValidatorDirective {
-    // Add your code here
+export class EmailValidatorDirective implements Validator {
+  // Add your code here
+  validate(control: AbstractControl): ValidationErrors | null {
+    return emailValidator()(control);
+  }
+}
+export function emailValidator(): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    const emailRegExp = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,4}$/;
+    const email = control.value;
+
+    if (email && !emailRegExp.test(email)) {
+      return { invalidEmail: true };
+    } else {
+      return null;
+    }
+  };
 }
