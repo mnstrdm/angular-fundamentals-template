0bd9beefb1a62ea2ebe1dcd0b5e79e29fb648ec0
“mnstrdm“ <mnstrdm@gmail.com>
2024-10-05 19:14:56 +0200

    feat: create form model and submit method

--- src/app/shared/components/registration-form/registration-form.component.ts
+++ src/app/shared/components/registration-form/registration-form.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { emailValidator } from "@app/shared/directives/email.directive";
 
 @Component({
-  selector: 'app-registration-form',
-  templateUrl: './registration-form.component.html',
-  styleUrls: ['./registration-form.component.scss'],
+  selector: "app-registration-form",
+  templateUrl: "./registration-form.component.html",
+  styleUrls: ["./registration-form.component.scss"],
 })
-export class RegistrationFormComponent {
+export class RegistrationFormComponent implements OnInit {
   registrationForm!: FormGroup;
-  // Use the names `name`, `email`, `password` for the form controls.
+
+  ngOnInit(): void {
+    this.registrationForm = new FormGroup({
+      name: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(6),
+      ]),
+      email: new FormControl(null, [Validators.required, emailValidator()]),
+      password: new FormControl(null, Validators.required),
+    });
+  }
+  submitted: boolean = false;
+
+  get name() {
+    return this.registrationForm.get("name");
+  }
+
+  get email() {
+    return this.registrationForm.get("email");
+  }
+
+  get password() {
+    return this.registrationForm.get("password");
+  }
+
+  onSubmit() {
+    if (this.registrationForm.valid) {
+      this.submitted = true;
+      this.registrationForm.reset();
+    }
+  }
 }
