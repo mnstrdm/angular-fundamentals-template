defdb022e2a8c71e63fa1b8dc208a3bfbc14399a
“mnstrdm“ <mnstrdm@gmail.com>
2024-10-05 19:29:35 +0200

    feat: create form submittion handler

--- src/app/shared/components/login-form/login-form.component.ts
+++ src/app/shared/components/login-form/login-form.component.ts
@@ -1,12 +1,22 @@
-import { Component, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component, ViewChild } from "@angular/core";
+import { NgForm } from "@angular/forms";
 
 @Component({
-  selector: 'app-login-form',
-  templateUrl: './login-form.component.html',
-  styleUrls: ['./login-form.component.scss'],
+  selector: "app-login-form",
+  templateUrl: "./login-form.component.html",
+  styleUrls: ["./login-form.component.scss"],
 })
 export class LoginFormComponent {
   @ViewChild("loginForm") public loginForm!: NgForm;
   //Use the names `email` and `password` for form controls.
+
+  onSubmit() {
+    this.loginForm.resetForm();
+  }
+  onLogin() {
+    if (this.loginForm.valid) {
+      const event = new Event("submit");
+      this.loginForm.onSubmit(event);
+    }
+  }
 }
