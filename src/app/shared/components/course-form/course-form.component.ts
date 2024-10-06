949cf24a0761690cad9bb380d91615f37a47340c
“mnstrdm“ <mnstrdm@gmail.com>
2024-10-06 22:30:16 +0200

    feat: created form model, validation and button logic

--- src/app/shared/components/course-form/course-form.component.ts
+++ src/app/shared/components/course-form/course-form.component.ts
@@ -1,19 +1,118 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 import {
-  FormBuilder, FormGroup
-} from '@angular/forms';
-import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
-import { fas } from '@fortawesome/free-solid-svg-icons';
+  FormBuilder,
+  FormGroup,
+  FormArray,
+  FormControl,
+  Validators,
+} from "@angular/forms";
+
+import { v4 } from "uuid";
+
+import { FaIconLibrary } from "@fortawesome/angular-fontawesome";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { mockedAuthorsList } from "@app/shared/mocks/mocks";
+import { Author } from "@app/shared/models/author.model";
 
 @Component({
-  selector: 'app-course-form',
-  templateUrl: './course-form.component.html',
-  styleUrls: ['./course-form.component.scss'],
+  selector: "app-course-form",
+  templateUrl: "./course-form.component.html",
+  styleUrls: ["./course-form.component.scss"],
 })
-export class CourseFormComponent {
+export class CourseFormComponent implements OnInit {
+  courseForm!: FormGroup;
+  authorsList: string[] = [];
+  courseAuthorsList: string[] = [];
+  submitted!: boolean;
+
   constructor(public fb: FormBuilder, public library: FaIconLibrary) {
     library.addIconPacks(fas);
+    this.buildForm();
   }
-  courseForm!: FormGroup;
+
+  ngOnInit(): void {
+    const authorsListArray = this.courseForm.get("authors") as FormArray;
+    const courseAuthorsListArray = this.courseForm.get(
+      "courseAuthors"
+    ) as FormArray;
+    this.authorsList = mockedAuthorsList.map((auth: Author) => auth.name);
+    this.authorsList.forEach((author) => {
+      authorsListArray.push(this.fb.control(author));
+    });
+    this.courseAuthorsList.forEach((author) => {
+      courseAuthorsListArray.push(this.fb.control(author));
+    });
+
+    this.submitted = false;
+  }
+
   // Use the names `title`, `description`, `author`, 'authors' (for authors list), `duration` for the form controls.
+  buildForm(): void {
+    this.courseForm = this.fb.group({
+      title: ["", [Validators.required, Validators.minLength(2)]],
+      description: ["", [Validators.required, Validators.minLength(2)]],
+      duration: ["", [Validators.required, Validators.min(0)]],
+      newAuthor: this.fb.group({
+        author: [
+          "",
+          [Validators.minLength(2), Validators.pattern("^[a-zA-Z0-9]*$")],
+        ],
+      }),
+      authors: this.fb.array([]),
+      courseAuthors: this.fb.array([]),
+    });
+  }
+  get title(): FormControl {
+    return this.courseForm.get("title") as FormControl;
+  }
+  get description(): FormControl {
+    return this.courseForm.get("description") as FormControl;
+  }
+  get duration(): FormControl {
+    return this.courseForm.get("duration") as FormControl;
+  }
+
+  get newAuthor(): FormGroup {
+    return this.courseForm.get("newAuthor") as FormGroup;
+  }
+  get author(): FormControl {
+    return this.newAuthor.get("author") as FormControl;
+  }
+
+  get authors(): FormArray {
+    return this.courseForm.get("authors") as FormArray;
+  }
+
+  get courseAuthors(): FormArray {
+    return this.courseForm.get("courseAuthors") as FormArray;
+  }
+
+  addAuthor(index: number): void {
+    this.courseAuthors.push(this.fb.control(this.authors.value[index]));
+    this.authors.removeAt(index);
+  }
+  deleteAuthor(index: number): void {
+    this.authors.push(this.fb.control(this.courseAuthors.value[index]));
+    this.courseAuthors.removeAt(index);
+  }
+
+  addNewAuthor(): void {
+    this.authors.push(this.fb.control({ id: v4(), name: this.author.value }));
+    this.newAuthor.reset();
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    if (this.courseForm.valid) {
+      console.log(
+        `Id: ${v4()}
+         Title: ${this.title.value},
+         description: ${this.description.value},
+         duration: ${this.duration.value},
+         Authors: ${this.courseAuthors.value}`
+      );
+      this.courseForm.reset();
+      this.submitted = false;
+    }
+  }
 }
