c9b86ca4b8240885b9eaed59d55c16060d7c93af
“mnstrdm“ <mnstrdm@gmail.com>
2024-10-13 20:48:13 +0200

    feat: add button handlers with routing to CoursesComponent

--- src/app/features/courses/courses.component.ts
+++ src/app/features/courses/courses.component.ts
@@ -1,8 +1,30 @@
 import { Component } from "@angular/core";
+import { Router } from "@angular/router";
+
+import { mockedCoursesList } from "@app/shared/mocks/mocks";
 
 @Component({
   selector: "app-courses",
   templateUrl: "./courses.component.html",
   styleUrls: ["./courses.component.scss"],
 })
-export class CoursesComponent {}
+export class CoursesComponent {
+  constructor(private router: Router) {}
+  mockedCoursesList = mockedCoursesList;
+
+  // info page text
+  infoTitle = "Your List Is Empty";
+  infoText = "Please use 'Add New Course' button to add your first course";
+
+  showCourse(courseId: string) {
+    this.router.navigate(["/courses", courseId]);
+    console.log("Clicked on Show Course with ID:", courseId);
+  }
+  deleteCourse(courseId: string) {
+    console.log("Clicked on Delete Course with ID:", courseId);
+  }
+  editCourse(courseId: string) {
+    this.router.navigate(["/courses/edit", courseId]);
+    console.log("Clicked on Edit Course with ID:", courseId);
+  }
+}
