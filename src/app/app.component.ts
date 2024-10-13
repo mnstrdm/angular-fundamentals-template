d986df3217228a9c91d1a645995ef7771cc7bbaf
“mnstrdm“ <mnstrdm@gmail.com>
2024-10-13 20:37:34 +0200

    chore: clean up app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from "@angular/core";
 
-import { mockedCoursesList } from "./shared/mocks/mocks";
 import { ButtonLabels } from "./shared/constants/button-labels";
 
 @Component({
@@ -14,22 +13,4 @@ export class AppComponent {
   //  text for buttons
   btnTextLogin: string = ButtonLabels.login;
   btnTextLogout: string = ButtonLabels.logout;
-  btnTextAddNewCourse: string = ButtonLabels.addNewCourse;
-
-  // info page text
-  infoTitle = "Your List Is Empty";
-  infoText = "Please use 'Add New Course' button to add your first course";
-
-  // Mocked Course
-  mockedCourse = mockedCoursesList[0];
-  mockedCoursesList = mockedCoursesList;
-  showCourse(courseId: string) {
-    console.log("Clicked on Show Course with ID:", courseId);
-  }
-  deleteCourse(courseId: string) {
-    console.log("Clicked on Delete Course with ID:", courseId);
-  }
-  editCourse(courseId: string) {
-    console.log("Clicked on Edit Course with ID:", courseId);
-  }
 }
