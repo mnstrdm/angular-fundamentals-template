03fd5eee1c0a76e94f9d03424142d68b14dc963e
“mnstrdm“ <mnstrdm@gmail.com>
2024-10-13 21:00:41 +0200

    feat: add login logic to LoginComponent

--- src/app/shared/components/login-form/login-form.component.ts
+++ src/app/shared/components/login-form/login-form.component.ts
@@ -1,6 +1,8 @@
 import { Component, ViewChild } from "@angular/core";
 import { NgForm } from "@angular/forms";
 import { ButtonLabels } from "@app/shared/constants/button-labels";
+import { AuthService } from "@app/auth/services/auth.service";
+import { User } from "@app/shared/models/user.model";
 
 @Component({
   selector: "app-login-form",
@@ -12,10 +14,23 @@ export class LoginFormComponent {
 
   btnTextLogin: string = ButtonLabels.login;
   submitted: boolean = false;
+  user!: User;
+
+  constructor(private authService: AuthService) {}
 
   onLogin() {
     this.submitted = true;
     if (this.loginForm.valid) {
+      this.user = {
+        email: this.loginForm.value.email,
+        password: this.loginForm.value.password,
+      };
+      console.log(this.user);
+      this.authService.login(this.user).subscribe({
+        next: (response) =>
+          console.log("Login successful with response: ", response),
+        error: (err) => console.log("login failed with error: ", err),
+      });
       this.loginForm.resetForm();
       this.submitted = false;
     }
