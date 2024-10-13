df06dabc9eeda7b1f45d98e42b5a36b415f9099b
“mnstrdm“ <mnstrdm@gmail.com>
2024-10-13 20:54:15 +0200

    chore: update SharedModule imports

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,7 +1,7 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { ModalComponent } from './components/modal/modal.component';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+import { ModalComponent } from "./components/modal/modal.component";
 import {
   HeaderComponent,
   ButtonComponent,
@@ -10,12 +10,14 @@ import {
   CourseCardComponent,
   LoginFormComponent,
   RegistrationFormComponent,
-  CourseFormComponent
+  CourseFormComponent,
 } from "./components";
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import { DurationPipe } from './pipes/duration.pipe';
-import { CustomDatePipe } from './pipes/custom-date.pipe';
-import { EmailValidatorDirective } from '@shared/directives/email.directive';
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { DurationPipe } from "./pipes/duration.pipe";
+import { CustomDatePipe } from "./pipes/custom-date.pipe";
+import { EmailValidatorDirective } from "@shared/directives/email.directive";
+import { RouterModule } from "@angular/router";
+import { AuthModule } from "@app/auth/auth.module";
 
 const components = [
   HeaderComponent,
@@ -29,7 +31,7 @@ const components = [
   CourseFormComponent,
   DurationPipe,
   CustomDatePipe,
-  EmailValidatorDirective
+  EmailValidatorDirective,
 ];
 
 @NgModule({
@@ -39,7 +41,9 @@ const components = [
     FontAwesomeModule,
     FormsModule,
     ReactiveFormsModule,
+    RouterModule,
+    AuthModule,
   ],
-  exports: [components]
+  exports: [components],
 })
-export class SharedModule { }
+export class SharedModule {}
