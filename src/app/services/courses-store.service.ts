2f3a4a1c384b932b1206ed4332d7ea7dac00e761
“mnstrdm“ <mnstrdm@gmail.com>
2024-10-15 00:10:11 +0200

    feat: creat courses/ courses-store services

--- src/app/services/courses-store.service.ts
+++ src/app/services/courses-store.service.ts
@@ -1,42 +1,165 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { Course, newCourse } from "@app/shared/models/course.model";
+import { BehaviorSubject, catchError, Observable, tap } from "rxjs";
+import { CoursesService } from "./courses.service";
+import { Author } from "@app/shared/models/author.model";
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: "root",
 })
 export class CoursesStoreService {
-    getAll(){
-        // Add your code here
-    }
+  private courses$$: BehaviorSubject<Course[]> = new BehaviorSubject<Course[]>(
+    []
+  );
+  private loading$$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(
+    false
+  );
+  private authors$$: BehaviorSubject<Author[]> = new BehaviorSubject<Author[]>(
+    []
+  );
+  public courses$: Observable<Course[]> = this.courses$$.asObservable();
+  public loading$: Observable<boolean> = this.loading$$.asObservable();
+  public authors$: Observable<Author[]> = this.authors$$.asObservable();
 
-    createCourse(course: any) { // replace 'any' with the required interface
-        // Add your code here
-    }
+  constructor(private coursesService: CoursesService) {}
+  getAll() {
+    this.loading$$.next(true);
+    this.coursesService
+      .getAll()
+      .pipe(
+        tap((response) => {
+          this.courses$$.next(response.result);
+          this.loading$$.next(false);
+        }),
+        catchError((error) => {
+          this.loading$$.next(false);
+          console.error("Error getting courses from server:", error);
 
-    getCourse(id: string) {
-        // Add your code here
-    }
+          return [];
+        })
+      )
+      .subscribe();
+  }
 
-    editCourse(id: string, course: any) { // replace 'any' with the required interface
-        // Add your code here
-    }
+  createCourse(course: newCourse) {
+    this.loading$$.next(true);
+    this.coursesService
+      .createCourse(course)
+      .pipe(
+        tap(() => {
+          this.getAll();
+          this.loading$$.next(false);
+        }),
+        catchError((error) => {
+          this.loading$$.next(false);
+          console.error("Error creating course:", error);
 
-    deleteCourse(id: string) {
-        // Add your code here
-    }
+          return [];
+        })
+      )
+      .subscribe();
+  }
 
-    filterCourses(value: string) {
-        // Add your code here
-    }
+  getCourse(id: string): Observable<any> {
+    return this.coursesService.getCourse(id);
+  }
 
-    getAllAuthors() {
-        // Add your code here
-    }
+  editCourse(id: string, course: Course) {
+    this.loading$$.next(true);
+    this.coursesService
+      .editCourse(id, course)
+      .pipe(
+        tap(() => {
+          this.getAll();
+          this.loading$$.next(false);
+        }),
+        catchError((error) => {
+          this.loading$$.next(false);
+          console.error("Error editing course:", error);
 
-    createAuthor(name: string) {
-        // Add your code here
-    }
+          return [];
+        })
+      )
+      .subscribe();
+  }
 
-    getAuthorById(id: string) {
-        // Add your code here
-    }
+  deleteCourse(id: string) {
+    this.loading$$.next(true);
+    this.coursesService.deleteCourse(id).pipe(
+      tap(() => {
+        this.getAll();
+        this.loading$$.next(false);
+      }),
+      catchError((error) => {
+        this.loading$$.next(false);
+        console.error("Error deleting course:", error);
+
+        return [];
+      })
+    ).subscribe;
+  }
+
+  filterCourses(value: string) {
+    // Add your code here
+  }
+
+  getAllAuthors() {
+    this.loading$$.next(true);
+    this.coursesService
+      .getAllAuthors()
+      .pipe(
+        tap((response) => {
+          this.authors$$.next(response.result);
+          this.loading$$.next(false);
+        }),
+        catchError((error) => {
+          this.loading$$.next(false);
+          console.error("Error getting authors from server:", error);
+
+          return [];
+        })
+      )
+      .subscribe();
+  }
+
+  createAuthor(name: string) {
+    this.loading$$.next(true);
+    this.coursesService
+      .createAuthor(name)
+      .pipe(
+        tap(() => {
+          this.getAllAuthors();
+          this.loading$$.next(false);
+        }),
+        catchError((error) => {
+          this.loading$$.next(false);
+          console.error("Error creating Author:", error);
+
+          return [];
+        })
+      )
+      .subscribe();
+  }
+
+  getAuthorById(id: string) {
+    return this.coursesService.getAuthorById(id);
+    /* let author: Author;
+    this.loading$$.next(true);
+    this.coursesService
+      .getAuthorById(id)
+      .pipe(
+        tap(() => {
+          this.loading$$.next(false);
+        })
+      )
+      .subscribe({
+        next: (response) => {
+          author = response.result;
+          return author;
+        },
+        error: (error) => {
+          console.error("Error creating Author:", error);
+        },
+      }); */
+  }
 }
