b53880390445568a77439e523d7ff2047ef9a20c
“mnstrdm“ <mnstrdm@gmail.com>
2024-10-15 00:09:10 +0200

    feat: deploy services for get courses

--- src/app/features/courses/courses.component.ts
+++ src/app/features/courses/courses.component.ts
@@ -1,24 +1,41 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
+import { CoursesStoreService } from "@app/services/courses-store.service";
 
-import { mockedCoursesList } from "@app/shared/mocks/mocks";
+import { Course } from "@app/shared/models/course.model";
+import { UserStoreService } from "@app/user/services/user-store.service";
 
 @Component({
   selector: "app-courses",
   templateUrl: "./courses.component.html",
   styleUrls: ["./courses.component.scss"],
 })
-export class CoursesComponent {
-  constructor(private router: Router) {}
-  mockedCoursesList = mockedCoursesList;
-
+export class CoursesComponent implements OnInit {
+  constructor(
+    private router: Router,
+    private coursesStoreService: CoursesStoreService,
+    private userStorageService: UserStoreService
+  ) {}
   // info page text
   infoTitle = "Your List Is Empty";
   infoText = "Please use 'Add New Course' button to add your first course";
 
+  courses: Course[] = [];
+  loading: boolean = false;
+
+  ngOnInit() {
+    this.userStorageService.getUser();
+    this.coursesStoreService.courses$.subscribe((courses) => {
+      this.courses = courses;
+    });
+    this.coursesStoreService.loading$.subscribe((isLoading) => {
+      this.loading = isLoading;
+    });
+    this.coursesStoreService.getAll();
+  }
+
   showCourse(courseId: string) {
     this.router.navigate(["/courses", courseId]);
-    console.log("Clicked on Show Course with ID:", courseId);
   }
   deleteCourse(courseId: string) {
     console.log("Clicked on Delete Course with ID:", courseId);
