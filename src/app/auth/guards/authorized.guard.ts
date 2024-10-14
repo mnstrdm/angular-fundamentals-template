b6ce3895131c16a13efcf73549919774af2af40c
“mnstrdm“ <mnstrdm@gmail.com>
2024-10-15 00:04:09 +0200

    feat: create authorized guard

--- src/app/auth/guards/authorized.guard.ts
+++ src/app/auth/guards/authorized.guard.ts
@@ -1,8 +1,27 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { Route, Router, UrlSegment, UrlTree } from "@angular/router";
+import { AuthService } from "../services/auth.service";
+import { Observable } from "rxjs";
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: "root",
 })
 export class AuthorizedGuard {
-    // Add your code here
+  // Add your code here
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canMatch(
+    route: Route,
+    segment: UrlSegment[]
+  ):
+    | boolean
+    | UrlTree
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree> {
+    if (this.authService.isAuthorised) {
+      return true;
+    } else {
+      return this.router.createUrlTree(["/login"]);
+    }
+  }
 }
