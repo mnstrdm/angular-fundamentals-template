566264432a82bb60781aa00629354d3313fa56ea
“mnstrdm“ <mnstrdm@gmail.com>
2024-10-15 00:05:36 +0200

    feat: add AuthSevice methods

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -5,6 +5,9 @@ import { SessionStorageService } from "./session-storage.service";
 import { User } from "@app/shared/models/user.model";
 import { tap } from "rxjs";
 
+import { Router } from "@angular/router";
+import { UserStoreService } from "@app/user/services/user-store.service";
+
 const TOKEN = "SESSION_TOKEN";
 
 @Injectable({
@@ -16,7 +19,9 @@ export class AuthService {
 
   constructor(
     private http: HttpClient,
-    private sessionStorageService: SessionStorageService
+    private sessionStorageService: SessionStorageService,
+    private router: Router,
+    private userStorageService: UserStoreService
   ) {
     this.isAuthorized$$ = new BehaviorSubject<boolean>(
       this.sessionStorageService.hasToken()
@@ -27,11 +32,13 @@ export class AuthService {
     // replace 'any' with the required interface
     // Add your code here
 
-    return this.http.post<any>("http://localhost:4000/login", user).pipe(
+    return this.http.post<any>(this.getLoginUrl(), user).pipe(
       tap((response) => {
         if (response && response.result) {
           this.sessionStorageService.setToken(response.result);
           this.isAuthorized$$.next(true);
+
+          this.router.navigate(["/courses"]);
         }
       })
     );
@@ -39,23 +46,37 @@ export class AuthService {
 
   logout(): void {
     // Add your code here
+    this.sessionStorageService.deleteToken();
+    this.isAuthorized$$.next(false);
+    this.router.navigate(["/login"]);
   }
 
-  register(user: any) {
+  register(user: User): Observable<any> {
     // replace 'any' with the required interface
     // Add your code here
+    return this.http.post<any>(this.getRegisterUrl(), user);
   }
 
   get isAuthorised() {
     // Add your code here. Get isAuthorized$$ value
-    return true;
+    return this.isAuthorized$$.getValue();
   }
 
   set isAuthorised(value: boolean) {
     // Add your code here. Change isAuthorized$$ value
+    this.isAuthorized$$.next(value);
   }
 
   getLoginUrl() {
     // Add your code here
+    return "http://localhost:4000/login";
+  }
+  getRegisterUrl() {
+    // Add your code here
+    return "http://localhost:4000/register";
+  }
+
+  getToken() {
+    return this.sessionStorageService.getToken();
   }
 }
