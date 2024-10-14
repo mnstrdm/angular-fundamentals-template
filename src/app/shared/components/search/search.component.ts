103c230d0ffca28b88c7d2b57f1012efcd3eb45b
“mnstrdm“ <mnstrdm@gmail.com>
2024-10-15 00:18:37 +0200

    feat: check isAdmin throught services

--- src/app/shared/components/search/search.component.ts
+++ src/app/shared/components/search/search.component.ts
@@ -1,6 +1,8 @@
-import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { Component, EventEmitter, Input, Output, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
+import { CoursesStoreService } from "@app/services/courses-store.service";
 import { ButtonLabels } from "@app/shared/constants/button-labels";
+import { UserStoreService } from "@app/user/services/user-store.service";
 
 @Component({
   selector: "app-search",
@@ -11,9 +13,19 @@ export class SearchComponent {
   searchTerm: string = "";
   btnTextSearch: string = ButtonLabels.search;
   btnTextAddNewCourse: string = ButtonLabels.addNewCourse;
+  isAdmin!: boolean;
 
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    private userStoreService: UserStoreService,
+    private courseStoreService: CoursesStoreService
+  ) {}
 
+  ngOnInit() {
+    this.userStoreService.isAdmin$.subscribe(
+      (isadmin) => (this.isAdmin = isadmin)
+    );
+  }
   @Input() placeholder: string = "Input text";
   @Output() search: EventEmitter<string> = new EventEmitter();
 
@@ -23,6 +35,7 @@ export class SearchComponent {
   }
 
   onAddNewCourse() {
+    this.courseStoreService.getAllAuthors();
     this.router.navigate(["/courses/add"]);
   }
 }
