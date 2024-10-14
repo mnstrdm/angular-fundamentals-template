3d6ae8ca3a6efad8df41bb02d492705f99235c58
“mnstrdm“ <mnstrdm@gmail.com>
2024-10-14 23:58:50 +0200

    feat: add guards for routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,35 +8,50 @@ import {
 import { CoursesListComponent } from "./features/courses/courses-list/courses-list.component";
 import { CoursesComponent } from "./features/courses/courses.component";
 import { CourseInfoComponent } from "./features/course-info/course-info.component";
+import { AuthorizedGuard } from "./auth/guards/authorized.guard";
+import { NotAuthorizedGuard } from "./auth/guards/not-authorized.guard";
+import { AdminGuard } from "./user/guards/admin.guard";
 
 export const routes: Routes = [
   /* Add your code here */
   {
     path: "login",
     component: LoginFormComponent,
+    canActivate: [NotAuthorizedGuard],
   },
-  { path: "registration", component: RegistrationFormComponent },
+  {
+    path: "registration",
+    component: RegistrationFormComponent,
+    canActivate: [NotAuthorizedGuard],
+  },
   {
     path: "courses",
     component: CoursesComponent,
+    canMatch: [AuthorizedGuard],
   },
   {
     path: "courses/add",
     component: CourseFormComponent,
+    canMatch: [AuthorizedGuard],
+    canActivate: [AdminGuard],
   },
   {
     path: "courses/:id",
     component: CourseInfoComponent,
+    canMatch: [AuthorizedGuard],
   },
   {
     path: "courses/edit/:id",
     component: CourseFormComponent,
+    canMatch: [AuthorizedGuard],
+    canActivate: [AdminGuard],
   },
 
   {
     path: "",
     redirectTo: "/courses",
     pathMatch: "full",
+    canMatch: [AuthorizedGuard],
   },
 ];
 
