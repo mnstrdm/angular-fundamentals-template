aa05e6931fc3dc3a35abf68b5a8a60fc32ce29b4
“mnstrdm“ <mnstrdm@gmail.com>
2024-10-15 00:06:54 +0200

    feat: replace mocked data with services

--- src/app/features/course-info/course-info.component.ts
+++ src/app/features/course-info/course-info.component.ts
@@ -3,15 +3,9 @@ import { ActivatedRoute } from "@angular/router";
 import { ButtonLabels } from "@app/shared/constants/button-labels";
 import { mockedCoursesList } from "@app/shared/mocks/mocks";
 import { Location } from "@angular/common";
+import { CoursesStoreService } from "@app/services/courses-store.service";
+import { Course } from "@app/shared/models/course.model";
 
-interface Course {
-  id: string;
-  title: string;
-  description: string;
-  creationDate: string;
-  duration: number;
-  authors: string[];
-}
 @Component({
   selector: "app-course-info",
   templateUrl: "./course-info.component.html",
@@ -21,14 +15,35 @@ export class CourseInfoComponent implements OnInit {
   course!: Course | undefined;
   btnTextBack: string = ButtonLabels.back;
   courseId: string | null = null;
+  courseAuthors!: string[];
+  authorNames!: string[];
 
-  constructor(private route: ActivatedRoute, private location: Location) {}
+  constructor(
+    private route: ActivatedRoute,
+    private location: Location,
+    private courseStoreService: CoursesStoreService
+  ) {}
 
   ngOnInit(): void {
     this.courseId = this.route.snapshot.paramMap.get("id");
-    this.course = mockedCoursesList.find(
-      (course) => course.id === this.courseId
-    );
+
+    if (this.courseId) {
+      this.courseStoreService.getCourse(this.courseId).subscribe({
+        next: (course) => {
+          this.course = course.result;
+          this.courseAuthors = course.result.authors;
+        },
+      });
+      //this.getAuthorById();
+    }
+  }
+
+  getAuthorById(): void {
+    this.courseAuthors.forEach((id) => {
+      this.courseStoreService.getAuthorById(id).subscribe((author) => {
+        this.authorNames.push(author.result.name);
+      });
+    });
   }
 
   onBack() {
