26195717d7deb662e89de7f91475d06dd31d974b
“mnstrdm“ <mnstrdm@gmail.com>
2024-10-15 00:05:04 +0200

    feat: create TokenInterceptor

--- src/app/auth/interceptors/token.interceptor.ts
+++ src/app/auth/interceptors/token.interceptor.ts
@@ -1,7 +1,43 @@
-import { Injectable } from '@angular/core';
-import { HttpInterceptor } from '@angular/common/http';
+import { Injectable } from "@angular/core";
+import {
+  HttpErrorResponse,
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from "@angular/common/http";
+import { AuthService } from "../services/auth.service";
+import { Router } from "@angular/router";
+import { catchError, Observable, throwError } from "rxjs";
 
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
-    // Add your code here
+  constructor(private authService: AuthService, private router: Router) {}
+
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    const token = this.authService.getToken();
+
+    let modReq = req;
+    if (token) {
+      modReq = req.clone({
+        setHeaders: {
+          Authorization: token,
+        },
+      });
+    }
+
+    return next.handle(modReq).pipe(
+      catchError((error: HttpErrorResponse) => {
+        if (error.status === 401) {
+          this.authService.logout();
+          this.router.navigate(["/login"]);
+        }
+
+        return throwError(() => error);
+      })
+    );
+  }
 }
