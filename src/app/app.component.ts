cc82bbf5df3192a5d792aa21376ca3974520f393
“mnstrdm“ <mnstrdm@gmail.com>
2024-10-15 00:02:13 +0200

    feat: add logout logic and admin/authorization check

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 import { ButtonLabels } from "./shared/constants/button-labels";
+import { AuthService } from "./auth/services/auth.service";
+import { UserStoreService } from "./user/services/user-store.service";
 
 @Component({
   selector: "app-root",
@@ -9,8 +11,28 @@ import { ButtonLabels } from "./shared/constants/button-labels";
 })
 export class AppComponent {
   title = "courses-app";
+  isAuthorized: boolean = false;
+  isAdmin!: boolean;
+  userName: string | null = "";
 
   //  text for buttons
   btnTextLogin: string = ButtonLabels.login;
   btnTextLogout: string = ButtonLabels.logout;
+
+  constructor(
+    public authService: AuthService,
+    private userStoreService: UserStoreService
+  ) {}
+
+  ngOnInit() {
+    this.userStoreService.isAdmin$.subscribe(
+      (isadmin) => (this.isAdmin = isadmin)
+    );
+    this.userStoreService.name$.subscribe((name) => (this.userName = name));
+  }
+
+  onLogout() {
+    console.log("LOGGED OUT");
+    this.authService.logout();
+  }
 }
