53005afd2b673d92d6a6e09143a33c7424a413dc
“mnstrdm“ <mnstrdm@gmail.com>
2024-10-15 00:03:42 +0200

    chore: add imports and providers for auth.module

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,12 +2,23 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { SessionStorageService } from "./services/session-storage.service";
 import { AuthService } from "./services/auth.service";
-import { HttpClientModule } from "@angular/common/http";
+import { HTTP_INTERCEPTORS, HttpClientModule } from "@angular/common/http";
 import { windowProvider } from "./services/window.provider";
+import { RouterModule } from "@angular/router";
+import { TokenInterceptor } from "./interceptors/token.interceptor";
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, HttpClientModule],
-  providers: [SessionStorageService, AuthService, windowProvider],
+  imports: [CommonModule, HttpClientModule, RouterModule],
+  providers: [
+    SessionStorageService,
+    AuthService,
+    windowProvider,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptor, // Register your TokenInterceptor
+      multi: true, // Allows multiple interceptors
+    },
+  ],
 })
 export class AuthModule {}
