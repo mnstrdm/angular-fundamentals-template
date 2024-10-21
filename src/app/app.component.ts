99cd57db9b418944987c5e0e92fdf18583fda730
“mnstrdm“ <mnstrdm@gmail.com>
2024-10-21 14:24:23 +0200

    refactor: authorisation, admin and user data in appComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,21 @@
-import { Component, OnInit } from "@angular/core";
+import { ChangeDetectorRef, Component, OnInit } from "@angular/core";
 
 import { ButtonLabels } from "./shared/constants/button-labels";
 import { AuthService } from "./auth/services/auth.service";
 import { UserStoreService } from "./user/services/user-store.service";
+import { User } from "./shared/models/user.model";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = "courses-app";
-  isAuthorized: boolean = false;
+  isAuthorized!: boolean;
   isAdmin!: boolean;
-  userName: string | null = "";
+  userName: string | undefined = "";
+  loggedInUser!: User;
 
   //  text for buttons
   btnTextLogin: string = ButtonLabels.login;
@@ -25,10 +27,21 @@ export class AppComponent {
   ) {}
 
   ngOnInit() {
-    this.userStoreService.isAdmin$.subscribe(
+    this.userStoreService.loggedInUser$.subscribe({
+      next: (user) => {
+        this.isAdmin = user.role === "admin" ? true : false;
+        this.userName = user.name;
+      },
+    });
+
+    this.authService.isAuthorized$.subscribe(
+      (isAuth) => (this.isAuthorized = isAuth)
+    );
+
+    /* this.userStoreService.isAdmin$.subscribe(
       (isadmin) => (this.isAdmin = isadmin)
     );
-    this.userStoreService.name$.subscribe((name) => (this.userName = name));
+    this.userStoreService.name$.subscribe((name) => (this.userName = name)); */
   }
 
   onLogout() {
