c63e26936d10b0713e7d6fc5562b9a18336d0232
“mnstrdm“ <mnstrdm@gmail.com>
2024-10-21 15:25:46 +0200

    feat: implement authors name display instead of id in CourseCard

--- src/app/shared/components/course-card/course-card.component.ts
+++ src/app/shared/components/course-card/course-card.component.ts
@@ -1,4 +1,11 @@
-import { Component, EventEmitter, Input, Output, OnInit } from "@angular/core";
+import {
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+  OnInit,
+  OnDestroy,
+} from "@angular/core";
 import {
   IconDefinition,
   faTrashCan,
@@ -7,12 +14,14 @@ import {
 import { Course } from "@app/shared/models/course.model";
 import { ButtonLabels } from "@app/shared/constants/button-labels";
 import { UserStoreService } from "@app/user/services/user-store.service";
+import { CoursesStoreService } from "@app/services/courses-store.service";
+import { Subscription, forkJoin } from "rxjs";
 @Component({
   selector: "app-course-card",
   templateUrl: "./course-card.component.html",
   styleUrls: ["./course-card.component.scss"],
 })
-export class CourseCardComponent {
+export class CourseCardComponent implements OnInit, OnDestroy {
   @Input() isEditabel!: boolean;
   @Input() course!: Course;
 
@@ -20,19 +29,43 @@ export class CourseCardComponent {
   @Output() clickOnDelete: EventEmitter<string> = new EventEmitter();
   @Output() clickOnEdit: EventEmitter<string> = new EventEmitter();
 
-  constructor(private userStorageService: UserStoreService) {}
+  constructor(
+    private userStoreService: UserStoreService,
+    private courseStoreService: CoursesStoreService
+  ) {}
 
   btnTextShowCourse: string = ButtonLabels.showCourse;
   isAdmin!: boolean;
+  authorsByName: String[] = [];
+  private subscriptions: Subscription = new Subscription();
+
   ngOnInit() {
-    this.userStorageService.isAdmin$.subscribe(
+    this.isAdmin = this.userStoreService.isAdmin;
+    /* this.userStoreService.loggedInUser$.subscribe(
+      (user) => (this.isAdmin = user.role === "admin" ? true : false)
+    ); */
+    /* this.userStoreService.isAdmin$.subscribe(
       (isadmin) => (this.isAdmin = isadmin)
-    );
+    ); */
+    this.createAuthorsNameList(this.course.authors);
   }
 
   // icons for buttons
   faTrashCan: IconDefinition = faTrashCan;
   faPencil: IconDefinition = faPencil;
+
+  createAuthorsNameList(authorsIdList: string[]): void {
+    const authorsObservables = authorsIdList.map((id) =>
+      this.courseStoreService.getAuthorById(id)
+    );
+
+    const sub = forkJoin(authorsObservables).subscribe(
+      (authors) =>
+        (this.authorsByName = authors.map((author) => author.result.name))
+    );
+
+    this.subscriptions.add(sub);
+  }
 
   onShowCourse() {
     this.clickOnShow.emit(this.course.id);
@@ -42,5 +75,9 @@ export class CourseCardComponent {
   }
   onEditCourse() {
     this.clickOnEdit.emit(this.course.id);
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
   }
 }
