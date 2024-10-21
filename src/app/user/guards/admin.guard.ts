7eb6998f3fbd031b90e71aa29d11578d355e0dfa
“mnstrdm“ <mnstrdm@gmail.com>
2024-10-21 15:33:28 +0200

    refactor: canActivate logic in AdminGuard

--- src/app/user/guards/admin.guard.ts
+++ src/app/user/guards/admin.guard.ts
@@ -1,26 +1,20 @@
 import { Injectable } from "@angular/core";
 import { UserStoreService } from "../services/user-store.service";
-import { Router, UrlTree } from "@angular/router";
-import { map, Observable } from "rxjs";
+import { CanActivate, Router, UrlTree } from "@angular/router";
+import { map, Observable, of } from "rxjs";
 
 @Injectable({
   providedIn: "root",
 })
-export class AdminGuard {
+export class AdminGuard implements CanActivate {
   constructor(
     private userStorageService: UserStoreService,
     private router: Router
   ) {}
 
   canActivate(): Observable<boolean | UrlTree> {
-    return this.userStorageService.isAdmin$.pipe(
-      map((isAdmin) => {
-        if (isAdmin) {
-          return true;
-        } else {
-          return this.router.createUrlTree(["/courses"]);
-        }
-      })
-    );
+    return this.userStorageService.isAdmin
+      ? of(true)
+      : of(this.router.createUrlTree(["/courses"]));
   }
 }
