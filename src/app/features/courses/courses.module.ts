f6585cb24b1215c4b07a87669d5b11383dc1402a
“mnstrdm“ <mnstrdm@gmail.com>
2024-10-21 15:19:44 +0200

    chore: update CoursesModule imports

--- src/app/features/courses/courses.module.ts
+++ src/app/features/courses/courses.module.ts
@@ -4,10 +4,17 @@ import { SharedModule } from "@app/shared/shared.module";
 import { CourseListModule } from "./courses-list/courses-list.module";
 import { RouterOutlet } from "@angular/router";
 import { CommonModule } from "@angular/common";
+import { CoursesRoutingModule } from "./courses-routing.module";
 
 @NgModule({
   declarations: [CoursesComponent],
-  imports: [SharedModule, CommonModule, CourseListModule, RouterOutlet],
+  imports: [
+    SharedModule,
+    CommonModule,
+    CourseListModule,
+    RouterOutlet,
+    CoursesRoutingModule,
+  ],
   exports: [CoursesComponent],
 })
 export class CoursesModule {}
