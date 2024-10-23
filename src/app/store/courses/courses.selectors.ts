e79343760f397809b9daace49544debc3dd4acb7
“mnstrdm“ <mnstrdm@gmail.com>
2024-10-23 21:50:30 +0200

    feat: create NgRx selectors for courses

--- src/app/store/courses/courses.selectors.ts
+++ src/app/store/courses/courses.selectors.ts
@@ -1,1 +1,37 @@
-// Add your code here
+import { createFeatureSelector, createSelector } from "@ngrx/store";
+import { CoursesState } from "./courses.reducer";
+
+export const selectCoursesFeature =
+  createFeatureSelector<CoursesState>("courses");
+
+export const isAllCoursesLoadingSelector = createSelector(
+  selectCoursesFeature,
+  (state: CoursesState) => state.isAllCoursesLoading
+);
+
+export const isSearchingStateSelector = createSelector(
+  selectCoursesFeature,
+  (state: CoursesState) => state.isSearchState
+);
+
+export const isSingleCourseLoadingSelector = createSelector(
+  selectCoursesFeature,
+  (state: CoursesState) => state.isSingleCourseLoading
+);
+
+export const getAllCourses = createSelector(
+  selectCoursesFeature,
+  (state: CoursesState) => state.allCourses
+);
+
+export const getCourses = getAllCourses;
+
+export const getCourse = createSelector(
+  selectCoursesFeature,
+  (state: CoursesState) => state.course
+);
+
+export const getErrorMessage = createSelector(
+  selectCoursesFeature,
+  (state: CoursesState) => state.errorMessage
+);
