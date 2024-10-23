7984b5640dcdd7e760a4d7563888fa39f796b3d6
“mnstrdm“ <mnstrdm@gmail.com>
2024-10-23 21:53:46 +0200

    refactor: change return value form response to response.result in some courses services

--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
+import { map, Observable } from "rxjs";
 import { Course } from "@app/shared/models/course.model";
 import { environment } from "src/environments/environment";
 
@@ -11,31 +11,37 @@ export class CoursesService {
   constructor(private http: HttpClient) {}
 
   getAll(): Observable<any> {
-    return this.http.get<any>(`${environment.serverApiUrl}/courses/all`);
+    return this.http
+      .get<any>(`${environment.serverApiUrl}/courses/all`)
+      .pipe(map((response) => response.result));
   }
 
   createCourse(course: Course): Observable<any> {
-    return this.http.post<any>(
-      `${environment.serverApiUrl}/courses/add`,
-      course
-    );
+    return this.http
+      .post<any>(`${environment.serverApiUrl}/courses/add`, course)
+      .pipe(map((response) => response.result));
   }
 
   editCourse(id: string, course: Course): Observable<any> {
-    return this.http.put<any>(
-      `${environment.serverApiUrl}/courses/${id}`,
-      course
-    );
+    return this.http
+      .put<any>(`${environment.serverApiUrl}/courses/${id}`, course)
+      .pipe(map((response) => response.result));
   }
 
   getCourse(id: string): Observable<any> {
     return this.http.get<any>(`${environment.serverApiUrl}/courses/${id}`);
   }
 
+  getSpecificCourse(id: string): Observable<any> {
+    return this.http
+      .get<any>(`${environment.serverApiUrl}/courses/${id}`)
+      .pipe(map((response) => response.result));
+  }
+
   deleteCourse(id: string): Observable<any> {
     return this.http.delete<any>(`${environment.serverApiUrl}/courses/${id}`);
   }
-
+  // maybe return the result here as well as getAll
   filterCourses(value: string): Observable<any> {
     return this.http.get(`${environment.serverApiUrl}/courses/filter?${value}`);
   }
