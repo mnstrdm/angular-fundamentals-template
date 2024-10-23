288b753ae155995f402389f6e7da4c28d061e4c8
“mnstrdm“ <mnstrdm@gmail.com>
2024-10-23 21:57:44 +0200

    feat: add Store and Effects Modules to app.module imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
+import { CommonModule } from "@angular/common";
+
+import { StoreModule } from "@ngrx/store";
+import { EffectsModule } from "@ngrx/effects";
+import { reducers, effects } from "./store";
+
 import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+
 import { SharedModule } from "@shared/shared.module";
-import { CourseInfoModule } from "./features/course-info/course-info.module";
+
 import { AppComponent } from "@app/app.component";
+
 import { NotAuthorizedGuard } from "@app/auth/guards/not-authorized.guard";
 import { AuthorizedGuard } from "@app/auth/guards/authorized.guard";
+
 import { CoursesStoreService } from "@app/services/courses-store.service";
 import { CoursesService } from "@app/services/courses.service";
+
+import { CourseInfoModule } from "./features/course-info/course-info.module";
 import { CourseListModule } from "./features/courses/courses-list/courses-list.module";
 import { CoursesModule } from "./features/courses/courses.module";
 import { AppRoutingModule } from "./app-routing.module";
 import { AuthModule } from "./auth/auth.module";
-import { CommonModule } from "@angular/common";
 import { LoginFormModule } from "./shared/components/login-form/login-form.module";
 import { RegistrationFormModule } from "./shared/components/registration-form/registration-form.module";
 
@@ -30,6 +40,8 @@ import { RegistrationFormModule } from "./shared/components/registration-form/re
     CommonModule,
     LoginFormModule,
     RegistrationFormModule,
+    StoreModule.forRoot(reducers),
+    EffectsModule.forRoot(effects),
   ],
   providers: [
     AuthorizedGuard,
