655a0c2d8015659f8311afc8a1930601accb3974
“mnstrdm“ <mnstrdm@gmail.com>
2024-10-24 11:22:46 +0200

    refactor: use Facades in load and searc functions in CoursesComponent

--- src/app/features/courses/courses.component.ts
+++ src/app/features/courses/courses.component.ts
@@ -3,7 +3,9 @@ import { Router } from "@angular/router";
 import { CoursesStoreService } from "@app/services/courses-store.service";
 
 import { Course } from "@app/shared/models/course.model";
+import { CoursesStateFacade } from "@app/store/courses/courses.facade";
 import { UserStoreService } from "@app/user/services/user-store.service";
+import { Observable } from "rxjs";
 
 @Component({
   selector: "app-courses",
@@ -11,10 +13,12 @@ import { UserStoreService } from "@app/user/services/user-store.service";
   styleUrls: ["./courses.component.scss"],
 })
 export class CoursesComponent implements OnInit {
+  courses$: Observable<Course[]> = this.coursesStateFacades.allCourses$;
   constructor(
     private router: Router,
     private coursesStoreService: CoursesStoreService,
-    private userStoreService: UserStoreService
+    private userStoreService: UserStoreService,
+    private coursesStateFacades: CoursesStateFacade
   ) {}
   // info page text
   infoTitle = "Your List Is Empty";
@@ -26,24 +30,35 @@ export class CoursesComponent implements OnInit {
   ngOnInit() {
     this.userStoreService.getUser();
     this.isEditable = this.userStoreService.isAdmin;
-    this.coursesStoreService.courses$.subscribe((courses) => {
+    /* this.coursesStoreService.courses$.subscribe((courses) => {
       this.courses = courses;
-    });
-
-    this.coursesStoreService.getAll();
+      
+    }); */
+    this.loadCourses();
+    //this.coursesStoreService.getAll();
   }
 
+  loadCourses() {
+    this.coursesStateFacades.getAllCourses();
+    this.courses$.subscribe((course) => (this.courses = course));
+  }
   showCourse(courseId: string) {
     this.router.navigate(["/courses", courseId]);
   }
   deleteCourse(courseId: string) {
-    this.coursesStoreService.deleteCourse(courseId);
+    //this.coursesStoreService.deleteCourse(courseId);
+    this.coursesStateFacades.deleteCourse(courseId);
   }
   editCourse(courseId: string) {
     this.router.navigate(["/courses/edit", courseId]);
   }
 
   onSearch(searchTerm: string) {
-    this.coursesStoreService.filterCourses(searchTerm);
+    if (searchTerm === "") {
+      this.loadCourses();
+    } else {
+      this.coursesStateFacades.getFilteredCourses(searchTerm);
+    }
+    //this.coursesStoreService.filterCourses(searchTerm);
   }
 }
