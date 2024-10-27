b8f639dd89477a8f46cde1298183fa67820fc397
“mnstrdm“ <mnstrdm@gmail.com>
2024-10-27 21:46:04 +0100

    feat: add user,authors and authentication feature stores

--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -1,13 +1,33 @@
 import { ActionReducerMap } from "@ngrx/store";
-import { CoursesState, coursesReducer } from "./courses/courses.reducer";
 import { CoursesEffects } from "./courses/courses.effects";
+import { UserEffects } from "./user/user.effects";
+import { AuthorsEffects } from "./author/authors.effects";
+import { CoursesState, coursesReducer } from "./courses/courses.reducer";
+import { userReducer, UserState } from "./user/user.reducer";
+import { authorsReducer, AuthorsState } from "./author/authors.reducer";
+import {
+  authenticationReducer,
+  AuthenticationState,
+} from "./authentication/authentication.reducer";
+import { AuthenticationEffects } from "./authentication/authentication.effects";
 
 export interface State {
   courses: CoursesState;
+  user: UserState;
+  authors: AuthorsState;
+  authentication: AuthenticationState;
 }
 
 export const reducers: ActionReducerMap<State> = {
   courses: coursesReducer,
+  user: userReducer,
+  authors: authorsReducer,
+  authentication: authenticationReducer,
 };
 
-export const effects = [CoursesEffects];
+export const effects = [
+  CoursesEffects,
+  UserEffects,
+  AuthorsEffects,
+  AuthenticationEffects,
+];
