f249d481277d2195a79f09c497d55c973134bd53
“mnstrdm“ <mnstrdm@gmail.com>
2024-10-27 22:03:33 +0100

    feat: implement Store usage in CourseInfo

--- src/app/features/course-info/course-info.component.ts
+++ src/app/features/course-info/course-info.component.ts
@@ -4,7 +4,7 @@ import { ButtonLabels } from "@app/shared/constants/button-labels";
 import { Location } from "@angular/common";
 import { CoursesStoreService } from "@app/services/courses-store.service";
 import { Course } from "@app/shared/models/course.model";
-import { forkJoin, Observable, Subscription } from "rxjs";
+import { map, Observable, Subscription } from "rxjs";
 import { CoursesStateFacade } from "@app/store/courses/courses.facade";
 
 @Component({
@@ -14,69 +14,54 @@ import { CoursesStateFacade } from "@app/store/courses/courses.facade";
 })
 export class CourseInfoComponent implements OnInit, OnDestroy {
   course!: Course | null;
-  course$: Observable<Course | null> = this.coursesStateFacades.course$;
+  course$: Observable<Course | null> = this.coursesStateFacade.course$;
   btnTextBack: string = ButtonLabels.back;
   courseId: string | null = null;
-  courseAuthors!: string[];
   authorsByName: string[] = [];
-  private subscriptions: Subscription = new Subscription();
+  private subscriptions: Subscription[] = [];
 
   constructor(
     private route: ActivatedRoute,
     private location: Location,
     private courseStoreService: CoursesStoreService,
-    private coursesStateFacades: CoursesStateFacade
+    private coursesStateFacade: CoursesStateFacade
   ) {}
 
   ngOnInit(): void {
     const courseId = this.route.snapshot.paramMap.get("id");
 
     if (courseId) {
-      this.coursesStateFacades.getSingleCourse(courseId);
-      this.course$.subscribe((course) => {
-        this.course = course;
-        console.log("course: ", course);
-        this.courseAuthors = course!.authors;
-        if (this.courseAuthors.length > 0) {
-          this.createAuthorsNameList(this.courseAuthors);
-        }
-      });
-    }
-
-    /* if (courseId) {
-      this.courseStoreService.getCourse(courseId).subscribe({
-        next: (course) => {
-          this.course = course.result;
-          this.courseAuthors = course.result.authors;
-          if (this.courseAuthors.length > 0) {
-            this.createAuthorsNameList(this.courseAuthors);
+      this.coursesStateFacade.getSingleCourse(courseId);
+      const subscribeIsSingleCourseLoading =
+        this.coursesStateFacade.isSingleCourseLoading$.subscribe(
+          (isCourseLoading) => {
+            !isCourseLoading &&
+              this.subscriptions.push(
+                this.course$.subscribe((course) => {
+                  this.course = course;
+                  const subscribeGetAuthorsById = this.courseStoreService
+                    .getAuthorsById(course!.authors)
+                    .pipe(
+                      map((authors) => authors.map((author) => author.name))
+                    )
+                    .subscribe(
+                      (authorsName) => (this.authorsByName = authorsName)
+                    );
+                  this.subscriptions.push(subscribeGetAuthorsById);
+                })
+              );
           }
-          console.log(this.authorsByName);
-        },
-        error: (err) => {
-          console.error("Hiba a kurzus adatainak betöltésekor:", err);
-        },
-      });
-    } */
-  }
-
-  createAuthorsNameList(authorsIdList: string[]): void {
-    const authorsObservables = authorsIdList.map((id) =>
-      this.courseStoreService.getAuthorById(id)
-    );
-
-    const sub = forkJoin(authorsObservables).subscribe(
-      (authors) =>
-        (this.authorsByName = authors.map((author) => author.result.name))
-    );
-
-    this.subscriptions.add(sub);
+        );
+      this.subscriptions.push(subscribeIsSingleCourseLoading);
+    }
   }
 
   onBack() {
     this.location.back();
   }
   ngOnDestroy(): void {
-    this.subscriptions.unsubscribe();
+    this.subscriptions.forEach((subscrition) => {
+      subscrition.unsubscribe();
+    });
   }
 }
