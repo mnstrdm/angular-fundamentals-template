e8621565c6644401d5943612532a5d56ff8dcf55
“mnstrdm“ <mnstrdm@gmail.com>
2024-10-27 21:33:52 +0100

    refactor: use storage value for guard in NotAuthorizedGuard

--- src/app/auth/guards/not-authorized.guard.ts
+++ src/app/auth/guards/not-authorized.guard.ts
@@ -1,29 +1,31 @@
 import { Injectable } from "@angular/core";
-import {
-  Router,
-  Route,
-  UrlSegment,
-  UrlTree,
-  CanActivate,
-} from "@angular/router";
-import { Observable } from "rxjs";
-import { AuthService } from "../services/auth.service";
+import { Router, UrlTree, CanActivate } from "@angular/router";
+import { map, Observable, take } from "rxjs";
+import { AuthenticationFacade } from "@app/store/authentication/authentication.facade";
 
 @Injectable({
   providedIn: "root",
 })
 export class NotAuthorizedGuard implements CanActivate {
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(
+    private router: Router,
+    private authenticationFacade: AuthenticationFacade
+  ) {}
 
   canActivate():
     | boolean
     | UrlTree
     | Observable<boolean | UrlTree>
     | Promise<boolean | UrlTree> {
-    if (!this.authService.isAuthorised) {
-      return true;
-    } else {
-      return this.router.createUrlTree(["/courses"]);
-    }
+    return this.authenticationFacade.isAuthorized$.pipe(
+      take(1),
+      map((isAuthorized) => {
+        if (!isAuthorized) {
+          return true;
+        } else {
+          return this.router.createUrlTree(["/courses"]);
+        }
+      })
+    );
   }
 }
