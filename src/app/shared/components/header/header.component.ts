f95d2763151fa3e9b031298cb1b83e56546806d0
“mnstrdm“ <mnstrdm@gmail.com>
2024-10-27 22:04:06 +0100

    chore: add authenticationFacade to Header

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,18 +1,17 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
 import { AuthService } from "@app/auth/services/auth.service";
+import { AuthenticationFacade } from "@app/store/authentication/authentication.facade";
 
 @Component({
   selector: "app-header",
   templateUrl: "./header.component.html",
   styleUrls: ["./header.component.scss"],
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
   isAuthorised: boolean = false;
 
-  constructor(public authService: AuthService) {}
-  ngOnInit(): void {
-    this.authService.isAuthorized$.subscribe(
-      (isAuth) => (this.isAuthorised = isAuth)
-    );
-  }
+  constructor(
+    public authService: AuthService,
+    public authenticationFacade: AuthenticationFacade
+  ) {}
 }
