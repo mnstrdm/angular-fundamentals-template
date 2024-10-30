3d0377504a1f652bf373377c3f3b0e7b1589ba85
“mnstrdm“ <mnstrdm@gmail.com>
2024-10-30 12:56:22 +0100

    refactor: change content projection to real content in Header

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,6 +1,9 @@
 import { Component } from "@angular/core";
 import { AuthService } from "@app/auth/services/auth.service";
+import { ButtonLabels } from "@app/shared/constants/button-labels";
 import { AuthenticationFacade } from "@app/store/authentication/authentication.facade";
+import { UserStateFacade } from "@app/store/user/user.facade";
+import { map, Observable } from "rxjs";
 
 @Component({
   selector: "app-header",
@@ -8,10 +11,19 @@ import { AuthenticationFacade } from "@app/store/authentication/authentication.f
   styleUrls: ["./header.component.scss"],
 })
 export class HeaderComponent {
-  isAuthorised: boolean = false;
+  isAuthorized$: Observable<boolean> = this.authService.isAuthorized$;
+  userName$: Observable<string | undefined> = this.userStateFacade.user$.pipe(
+    map((user) => user?.name)
+  );
+  btnTextLogout: string = ButtonLabels.logout;
 
   constructor(
-    public authService: AuthService,
-    public authenticationFacade: AuthenticationFacade
+    private authService: AuthService,
+    private userStateFacade: UserStateFacade,
+    private authenticationFacade: AuthenticationFacade
   ) {}
+
+  onLogout() {
+    this.authenticationFacade.logout();
+  }
 }
