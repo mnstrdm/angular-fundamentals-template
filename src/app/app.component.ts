2e514f412a6a3640a76e11aba9ec75f1674b28b2
“mnstrdm“ <mnstrdm@gmail.com>
2024-10-30 12:47:09 +0100

    chore: remove unnecessary lines

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,41 +1,8 @@
-import { Component, OnInit } from "@angular/core";
-
-import { ButtonLabels } from "./shared/constants/button-labels";
-import { AuthService } from "./auth/services/auth.service";
-import { UserStoreService } from "./user/services/user-store.service";
-import { AuthenticationFacade } from "./store/authentication/authentication.facade";
-import { UserStateFacade } from "./store/user/user.facade";
-import { map, Observable } from "rxjs";
+import { Component } from "@angular/core";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"],
 })
-export class AppComponent implements OnInit {
-  title = "courses-app";
-  //isAuthorized!: boolean;
-  isAdmin$: Observable<boolean> = this.userStateFacade.isAdmin$;
-  userName: string | undefined = "";
-  userName$: Observable<string | undefined> = this.userStateFacade.user$.pipe(
-    map((user) => user?.name)
-  );
-  //loggedInUser!: User;
-
-  //  text for buttons
-  btnTextLogin: string = ButtonLabels.login;
-  btnTextLogout: string = ButtonLabels.logout;
-
-  constructor(
-    public authService: AuthService,
-    private userStoreService: UserStoreService,
-    private userStateFacade: UserStateFacade,
-    private authenticationFacade: AuthenticationFacade
-  ) {}
-
-  ngOnInit() {}
-
-  onLogout() {
-    this.authenticationFacade.logout();
-  }
-}
+export class AppComponent {}
