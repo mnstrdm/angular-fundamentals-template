548c78774759da953dc58e9906cb54f917a7867a
“mnstrdm“ <mnstrdm@gmail.com>
2024-10-30 13:02:11 +0100

    refactor: move getAuthorsById fn to AuthorsFacade

--- src/app/store/author/authors.facade.ts
+++ src/app/store/author/authors.facade.ts
@@ -3,6 +3,8 @@ import * as AuthorsActions from "./authors.actions";
 import * as AuthorsSelectors from "./authors.selectors";
 import { AuthorsState } from "./authors.reducer";
 import { select, Store } from "@ngrx/store";
+import { Author } from "@app/shared/models/author.model";
+import { map, Observable } from "rxjs";
 
 @Injectable({
   providedIn: "root",
@@ -26,10 +28,19 @@ export class AuthorsStateFacade {
   getAllAuthors(): void {
     this.store.dispatch(AuthorsActions.requestAllAuthors());
   }
-  getAuthorsById(authors: string[]): void {
+  /* getAuthorsById(authors: string[]): void {
     this.store.dispatch(AuthorsActions.requestAuthorsById({ authors }));
-  }
+  } */
   createAuthor(name: string): void {
     this.store.dispatch(AuthorsActions.requestCreateAuthor({ name }));
+  }
+  // we can use this if we want to get a list of authors by a list of author Id-s,
+  // useing the allAuthors$ form our Authors Feature Store
+  getAuthorsById(authorsIdList: string[]): Observable<Author[]> {
+    return this.allAuthors$.pipe(
+      map((authors) =>
+        authors.filter((author) => authorsIdList.includes(author.id))
+      )
+    );
   }
 }
